dce40011b8c4b7997fee7c3cf95f25b61690f85c
Igor Gabriel Garcia Pereira <40238753+IgGarcia1@users.noreply.github.com>
2019-11-01 15:39:45 -0300

    Fixed Bug - POST Request
    Atualizando captura de requisições POST

--- User/create.php
+++ User/create.php
@@ -2,18 +2,17 @@
 
 
 header("Access-Control-Allow-Origin: *");
-//header("Content-Type: application/json; charset=UTF-8");
-//header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-//header("Access-Control-Max-Age: 3600");
-//header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST"); // "Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE"
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require_once('../dao/UserDAO.php');
-var_dump($_POST);
 
-if (isset($_POST) && $_POST != null){
-	$dados = json_decode($_POST);
+$pre_dados = file_get_contents("php://input");
+if (isset($pre_dados) && $pre_dados!=null){
+	$dados = json_decode($pre_dados, true);
 	var_dump($dados);
 }else{
-	echo "Pagina no ar";
+	print_r(json_encode( array('msg' => "Dados inválidos")));
 }
